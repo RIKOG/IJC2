0353a40c1016933bcf4863398429880e39f6f80a
Richard Gajdošík <47507877+RIKOG@users.noreply.github.com>
2021-04-16 19:33:30 +0200

    Sctrut chaining works
    Updated the main while cycle to correctly find last 10 line

--- tail.c
+++ tail.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[]) {
 
     LINE *head = NULL;
     LINE *line_ptr = (LINE *) malloc(sizeof(LINE));
+    LINE *tmp_ptr = NULL;
     // We want to be able to find the end of the struct chain thanks to the last pointer being NULL
     line_ptr->p_next_line = NULL;
 
@@ -28,19 +29,29 @@ int main(int argc, char *argv[]) {
                 head = line_ptr;
             }
             // If we found the needed amount of lines, we just want to "move" them along the chain backwards
-            if (n_of_lines_struct == n_of_lines_argument){
-                //tu chcem odstranit prvy struct a pripojit dalsi na konci retaze
+            if (n_of_lines_struct >= n_of_lines_argument){
+                tmp_ptr = head->p_next_line;
+                free(head);
+                head = tmp_ptr;
             }
-            n_of_lines_struct++;
             // todo there will be a problem when the text file will end with \n and then right away with EOF, there is getc because we dont want \n written in our struct
             // todo write a limiter for the maximum amount of characters in one line
-            line_ptr
+            // todo malloc another struct when theres EOF right after the \n? surely not fix it!!
+            n_of_lines_struct++;
+            tmp_ptr = line_ptr;
+            line_ptr = (LINE *) malloc(sizeof(LINE));
+            line_ptr->p_next_line = NULL;
+            // We connect our newly created struct to the last one
+            tmp_ptr->p_next_line = line_ptr;
             c = getc;
+            i = 0;
         }
         line_ptr->line[i++] = c;
-
-//        printf("%c", c);
-
+    }
+    tmp_ptr = head;
+    while(tmp_ptr->p_next_line != NULL){
+        printf("%s\n", tmp_ptr->line);
+        tmp_ptr = tmp_ptr->p_next_line;
     }
     return 0;
 }
